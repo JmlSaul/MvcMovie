d558f8709b531545cd4f827edd53a7437f1c83e6
jmlsaul <jmlsaul@live.com>
2018-02-08 11:01:28 +0800

    add

---
+++ CustomTagHelpers.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MvcMovie
+{
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "contoso.com";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";                             // Replaces <email> with <a> tag
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
